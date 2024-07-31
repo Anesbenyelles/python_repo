a36ac2850178f295ee2c950f4bb05588aa331166
Anes benyelles <anesbenyelles006@gmail.com>
2024-07-31 11:50:28 +0100

    add learn fils has some cheatcheet

---
+++ learn.py
@@ -0,0 +1,16 @@
+# data type---------------------------
+# type() fonction build in
+print(type(10))
+print(type("anes"))
+print(type('A'))
+print(type(10.5))
+print(type(-10))
+print(type([1,2,3,4]))
+print(type((1,2,3,4)))
+#variables---------------------------------
+var1=10
+print(var1)
+var1="aniss"
+print(var1)
+#resreved keyword----------
+#     help("keywords")
--- main.py
+++ main.py
@@ -1,2 +1,2 @@
 print("anes chbb   ")
-   
+#hiiiii
